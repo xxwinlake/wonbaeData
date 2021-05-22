494d28a72c50ba36c3abd7eaa611a0bfc9aaa0af
Jangwonbae <84276491+Jangwonbae@users.noreply.github.com>
2021-05-22 15:04:39 +0900

    프래그먼트 추가
    제곧내

--- app/src/main/java/com/example/wonbaeteamtest/ListFragment.java
+++ app/src/main/java/com/example/wonbaeteamtest/ListFragment.java
@@ -1,5 +1,7 @@
 package com.example.wonbaeteamtest;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +9,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ListFragment extends Fragment {
+    public interface OnListSelectedListener {
+        public void onListSelected(int i,ArrayList<ShelterData>mData);
+    }
+    private ArrayList<ShelterData> mData = new ArrayList<ShelterData>();//원본 arraylist
+    private ArrayList<ShelterData> arraylist;//mData 복사본
+    private ListView mList;
+    private MyAdapter mAdapter;
 
     public ListFragment() {
         // Required empty public constructor
@@ -17,7 +30,76 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View rv = inflater.inflate(R.layout.fragment_list, container, false);
+        /*리스트뷰 초기회*/
+        mList = (ListView) rv.findViewById(R.id.shetList);
+        mAdapter = new MyAdapter(getActivity(), mData);
+        mList.setAdapter(mAdapter);
+        arraylist = new ArrayList<ShelterData>();
+        arraylist.addAll(mData);//복사본 만들기
+        // 리스트의 모든 데이터를 arraylist에 복사한다.// mdata 복사본을 만든다.
+
+        //리스트뷰 클릭 이벤트
+        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Activity activity = getActivity();
+                ((OnListSelectedListener) activity).onListSelected(position,mData);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        return rv;
+    }
+    public void setNotify(){
+
+    }
+    /* Select 스피너 클릭메소드 구현*/
+    public void Select(int pos) {
+        mData.clear();
+        if (pos == 3) {
+            mData.addAll(arraylist);
+        } else {
+            for (int i = 0; i < arraylist.size(); i++) {
+                //객체의 subject(스피너의 인덱스)가 pos(메인 스피너에서 선택된 인덱스)이면
+                if (arraylist.get(i).subject == pos) {
+                    // 그 객체를 mData에 넣겠다
+                    mData.add(arraylist.get(i));
+                }
+            }
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+    /*search 서치뷰기능 메소드 */
+    public void search(String charText) {
+        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
+        mData.clear();
+
+        // 문자 입력이 없을때는 모든 데이터를 보여준다.
+        if (charText.length() == 0) {
+            mData.addAll(arraylist);
+        }
+        // 문자 입력을 할때..
+        else {
+            // 리스트의 모든 데이터를 검색한다.
+            for (int i = 0; i < arraylist.size(); i++) {
+                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
+                if (arraylist.get(i).name.contains(charText)) {
+                    // 검색된 데이터를 리스트에 추가한다.
+                    mData.add(arraylist.get(i));
+                }
+            }
+        }
+        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
+        mAdapter.notifyDataSetChanged();
+    }
+ public void setmData(ArrayList<ShelterData> mainMData){
+        mData.clear();
+        mData.addAll(mainMData);
+ }
+
+    public void setArraylist(ArrayList<ShelterData> mainArrayList){
+        arraylist.clear();
+        arraylist.addAll(mainArrayList);
     }
 }
--- app/src/main/java/com/example/wonbaeteamtest/MainActivity.java
+++ app/src/main/java/com/example/wonbaeteamtest/MainActivity.java
@@ -25,14 +25,22 @@ import android.widget.Toast;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ListFragment.OnListSelectedListener{
+    public void onListSelected(int position,ArrayList<ShelterData> mData){
+        mData1=mData;
+        /*리스트 뷰가 눌리면 해당 객체 정보를 대피소 보기 엑티비티로 보냄*/
+        Intent intent = new Intent(this, ShelterInpo.class);
+        intent.putExtra("position", position);
+        putExtraInfo(intent, mData1.get(position).subject, mData1.get(position).name,
+                mData1.get(position).address, mData1.get(position).provider);
+        startActivityForResult(intent, 0);
+    }
 
-    private ArrayList<ShelterData> mData = new ArrayList<ShelterData>();//원본 arraylist
-    private ArrayList<ShelterData> arraylist;//mData 복사본
+    ListFragment listFragment = new ListFragment();
+
+private ArrayList<ShelterData>mData1=new ArrayList<>();
+    private ArrayList<ShelterData>arraylist1=new ArrayList<>();
     private ArrayList<String> Subject = new ArrayList<>(); //스피너 생성용 배열
-
-    private ListView mList;
-    private MyAdapter mAdapter;
 
     private Spinner mSpinner;
 
@@ -56,38 +64,16 @@ public class MainActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
     }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         /*툴바생성*/
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("") ;
         super.setSupportActionBar(toolbar);
 
-        /*리스트뷰 초기회*/
-        mList = (ListView) findViewById(R.id.shetList);
-        mAdapter = new MyAdapter(this, mData);
-        mList.setAdapter(mAdapter);
-        arraylist = new ArrayList<ShelterData>();
-        arraylist.addAll(mData);//복사본 만들기
-
-        // 리스트의 모든 데이터를 arraylist에 복사한다.// mdata 복사본을 만든다.
-
-        //리스트뷰 클릭 이벤트
-        mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                /*리스트 뷰가 눌리면 해당 객체 정보를 대피소 보기 엑티비티로 보냄*/
-                Intent intent = new Intent(view.getContext(), ShelterInpo.class);
-                intent.putExtra("position", position);
-                putExtraInfo(intent, mData.get(position).subject, mData.get(position).name, mData.get(position).address, mData.get(position).provider);
-                startActivityForResult(intent, 0);
-            }
-        });
-
+        getSupportFragmentManager().beginTransaction().replace(R.id.list,listFragment).commit();
         //스피너 사용을 위한 Subject 배열에 요소추가
         Subject.add("지진 대피소");
         Subject.add("해일 대피소");
@@ -106,13 +92,11 @@ public class MainActivity extends AppCompatActivity {
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Select(position);
+                listFragment.Select(position);
                 subjectPosition = position;
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
             }
         });
     }
@@ -120,23 +104,6 @@ public class MainActivity extends AppCompatActivity {
         mSpinner.setSelection(3);
         subjectPosition=3;
     }
-    /* Select 스피너 클릭메소드 구현*/
-    public void Select(int pos) {
-        mData.clear();
-        if (pos == 3) {
-            mData.addAll(arraylist);
-        } else {
-            for (int i = 0; i < arraylist.size(); i++) {
-                //객체의 subject(스피너의 인덱스)가 pos(메인 스피너에서 선택된 인덱스)이면
-                if (arraylist.get(i).subject == pos) {
-                    // 그 객체를 mData에 넣겠다
-                    mData.add(arraylist.get(i));
-                }
-            }
-        }
-        mAdapter.notifyDataSetChanged();
-    }
-
     /*액션바에 메뉴구현 (서치뷰사용포함)*/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -147,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         //menuItem을 이용해서 SearchView 변수 생성
         SearchView sv = (SearchView) mSearch.getActionView();
         sv.setSubmitButtonEnabled(true);//확인버튼 활성화
-        sv.setQueryHint("대피소 이름 검색");
+        sv.setQueryHint("전체에서 이름 검색");
 
         //SearchView의 검색 이벤트
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -155,43 +122,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 selectSP();//검색시 전체보기로 초기세팅값을 설정
-                search(query);
+                listFragment.search(query);
                 return true;
             }
             //텍스트가 바뀔때마다 호출
             @Override
             public boolean onQueryTextChange(String newText) {
                 selectSP();//검색시 전체보기로 초기세팅값을 설정
-                search(newText);
+                listFragment.search(newText);
                 return true;
             }
         });
         return true;
     }
 
-  /*search 서치뷰기능 메소드 */
-    public void search(String charText) {
-        // 문자 입력시마다 리스트를 지우고 새로 뿌려준다.
-        mData.clear();
-
-        // 문자 입력이 없을때는 모든 데이터를 보여준다.
-        if (charText.length() == 0) {
-            mData.addAll(arraylist);
-        }
-        // 문자 입력을 할때..
-        else {
-            // 리스트의 모든 데이터를 검색한다.
-            for (int i = 0; i < arraylist.size(); i++) {
-                // arraylist의 모든 데이터에 입력받은 단어(charText)가 포함되어 있으면 true를 반환한다.
-                if (arraylist.get(i).name.contains(charText)) {
-                    // 검색된 데이터를 리스트에 추가한다.
-                    mData.add(arraylist.get(i));
-                }
-            }
-        }
-        // 리스트 데이터가 변경되었으므로 아답터를 갱신하여 검색된 데이터를 화면에 보여준다.
-        mAdapter.notifyDataSetChanged();
-    }
 
     /* 액션바에서 추가버튼 눌렸을 시 */
     @Override
@@ -204,48 +148,44 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
     /*인텐트이동*/
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         int position;
         if (requestCode == 1 && resultCode == RESULT_OK) {//추가를 누르고 편집엑티비티에서 저장을 누를경우 값들을 객체에 저장하고 리스트뷰 추가
-
-
-            mData.add(new ShelterData(R.drawable.testpic, data.getIntExtra("subject", -1), data.getStringExtra("name"),
+            mData1.add(new ShelterData(R.drawable.testpic, data.getIntExtra("subject", -1), data.getStringExtra("name"),
                     data.getStringExtra("provider"), data.getStringExtra("address")));
-            arraylist.add(new ShelterData(R.drawable.testpic, data.getIntExtra("subject", -1), data.getStringExtra("name"),
+            arraylist1.add(new ShelterData(R.drawable.testpic, data.getIntExtra("subject", -1), data.getStringExtra("name"),
                     data.getStringExtra("provider"), data.getStringExtra("address")));//mData에 객체가 추가되면 복사본도 추가
-
 
         } else if (requestCode == 0 && resultCode == 2) {//대피소 보기 엑티비티에서 삭제버튼을 눌렀을 경우 해당 객체를 삭제
             position = data.getIntExtra("position", -1);
             if(subjectPosition==3){
-                arraylist.remove(position);}
+                arraylist1.remove(position);}
             else{
                 int count=0;
-                for(int i = 0;i < arraylist.size(); i++)
+                for(int i = 0;i < arraylist1.size(); i++)
                 {
-                    if(arraylist.get(i).subject==subjectPosition){
+                    if(arraylist1.get(i).subject==subjectPosition){
                         if(count==position){
-                            arraylist.remove(i);
+                            arraylist1.remove(i);
                         }
                         count++;
                     }
                 }
             }
-            mAdapter.notifyDataSetChanged();
         }
-
         else if (requestCode == 0 && resultCode == 3) {//대피소 편집 엑티비티에서 저장을 누를 경우 객체 정보를 갱신
             position = data.getIntExtra("position", -1);
-            mData.get(position).subject = data.getIntExtra("subject", -1);
-            mData.get(position).name = data.getStringExtra("name");
-            mData.get(position).address = data.getStringExtra("address");
-            mData.get(position).provider = data.getStringExtra("provider");
+            mData1.get(position).subject = data.getIntExtra("subject", -1);
+            mData1.get(position).name = data.getStringExtra("name");
+            mData1.get(position).address = data.getStringExtra("address");
+            mData1.get(position).provider = data.getStringExtra("provider");
         }
-        Select(subjectPosition);
+        listFragment.setmData(mData1);
+        listFragment.setArraylist(arraylist1);
+        listFragment.Select(subjectPosition);
         super.onActivityResult(requestCode, resultCode, data);
     }
 
